532a2a826304eb33919c981c7f1d30a6a78c258f
Bredis <77401763+Bredis79@users.noreply.github.com>
2024-01-21 09:22:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const toggle = document.querySelector("#toggle")
+let toggleLabel = document.querySelector(".toggle--dark")
+let pattern = document.querySelector(".bg-pattern")
+let body = document.querySelector(".body__dark")
+let heading5 = document.querySelector("h5")
+let mainFollowersCard = document.querySelectorAll(".main__followers--media") 
+let mainLikesCard = document.querySelectorAll(".main__likes--card")
+let cardText = document.querySelectorAll(".card__text") 
+let facebookAccount = document.querySelector(".facebook--account")
+let twitterAccount = document.querySelector(".twitter--account") 
+let instagramAccount = document.querySelector(".instagram--account")
+let youtubeAccount = document.querySelector(".youtube--account") 
+
+
+toggle.addEventListener("click", function(e){
+    pattern.classList.toggle("bg-pattern-L")
+    body.classList.toggle("body__light")
+    heading5.classList.toggle("light-theme")
+    //cardText.classList.toggle("card__text-L")
+    toggleLabel.classList.toggle("toggle--light")
+
+    facebookAccount.classList.toggle("facebook--account-L")
+    twitterAccount.classList.toggle("twitter--account-L")
+    instagramAccount.classList.toggle("instagram--account-L")
+    youtubeAccount.classList.toggle("youtube--account-L")
+    darkThemeText.classList.toggle("light-theme")
+
+     //change color in light mode for all divs with class "main__followers--media"
+    for (let i = 0; i < mainFollowersCard.length; i++) {
+          mainFollowersCard[i].classList.toggle("main__followers--media-L")
+        }
+        
+    //change color in light mode for all divs with class "main__likes--card"
+    for (let i = 0; i < mainLikesCard.length; i++) {
+        mainLikesCard[i].classList.toggle("main__likes--card-L")
+        }
+    
+    //change color in light mode for all divs with class "main__likes--card"
+    for (let i = 0; i < cardText.length; i++) {
+        cardText[i].classList.toggle("card__text-L")
+        }
+
+
+console.log(darkThemeText)
+   
+})
